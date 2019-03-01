9e8332ca1a333e36d70c6788689a01d30c7a40ff
ME-SKY <meskalitofly@gmail.com>
2019-03-01 20:28:28 +1000

    Add files, add rxjs, change components and styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {fromEvent} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-test-project';
+export class AppComponent implements OnInit, OnDestroy {
+  observableWindow: any;
+
+  ngOnInit(): void {
+    const managePanel = document.getElementById('manage_panel');
+    this.observableWindow = fromEvent(window, 'scroll').subscribe(() => {
+        if (window.pageYOffset > managePanel.offsetTop) {
+          managePanel.classList.add('shadow_down', 'sticky');
+        } else {
+          managePanel.classList.remove('shadow_down', 'sticky');
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.observableWindow.unsubscribe();
+  }
 }
