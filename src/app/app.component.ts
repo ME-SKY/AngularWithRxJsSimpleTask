a8f33299957ecdb6e7dab4a1289736ab2a849170
ME-SKY <meskalitofly@gmail.com>
2019-03-06 19:06:25 +1000

    Add properly condition to #manage_panel show/hide.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {fromEvent} from 'rxjs';
+import {filter, pairwise, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,26 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     const managePanel = document.getElementById('manage_panel');
-    this.observableWindow = fromEvent(window, 'scroll').subscribe(() => {
-        if (window.pageYOffset > managePanel.offsetTop) {
-          managePanel.classList.add('shadow_down', 'sticky');
-        } else {
-          managePanel.classList.remove('shadow_down', 'sticky');
-        }
+    const editor = document.getElementById('editor');
+    const scrollStartPoint = editor.offsetTop - managePanel.clientHeight;
+
+    this.observableWindow = fromEvent(window, 'scroll')
+      .pipe(
+        map(() => window.pageYOffset),
+        pairwise(),
+        filter((pair) => {
+          if (pair[0] < pair[1]) {
+            return (pair[0] > scrollStartPoint && pair[1] > scrollStartPoint);
+          } else {
+            return pair[0] < editor.offsetTop && pair[1] < editor.offsetTop;
+          }
+        }))
+      .subscribe((e) => {
+        if (window.pageYOffset >= managePanel.offsetTop) {
+            managePanel.classList.add('shadow_down', 'sticky');
+          } else {
+            managePanel.classList.remove('shadow_down', 'sticky');
+          }
       }
     );
   }
